18e5c5370a416d0d01177c80072b18d7b815a71a
tarique vieira ramos <tariquevieira@gmail.com>
2022-06-18 10:32:30 -0300

    feat(crud_restaurante): add crud restaurante

--- src/paginas/Administracao/Restaurantes/AdministracaoRestaurantes.tsx
+++ src/paginas/Administracao/Restaurantes/AdministracaoRestaurantes.tsx
@@ -1,4 +1,5 @@
 import {
+  Button,
   Table,
   TableBody,
   TableCell,
@@ -8,6 +9,7 @@ import {
 } from '@mui/material';
 import axios from 'axios';
 import { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
 import IRestaurante from '../../../interfaces/IRestaurante';
 
 const AdministracaoRestaurantes = () => {
@@ -17,18 +19,49 @@ const AdministracaoRestaurantes = () => {
       .get('http://localhost:8000/api/v2/restaurantes/')
       .then((resposta) => setRestaurantes(resposta.data));
   }, []);
+
+  const excluir = (restauranteParaExcluir: IRestaurante) => {
+    axios
+      .delete<IRestaurante>(
+        `http://localhost:8000/api/v2/restaurantes/${restauranteParaExcluir.id}/`
+      )
+      .then((response) => {
+        const listaRestaurante = restaurantes.filter(
+          (restaurante) => restaurante.id !== restauranteParaExcluir.id
+        );
+        setRestaurantes([...listaRestaurante]);
+      });
+  };
+
   return (
     <TableContainer>
       <Table>
         <TableHead>
           <TableRow>
             <TableCell>Nome</TableCell>
+            <TableCell>Editar</TableCell>
+            <TableCell>Excluir</TableCell>
           </TableRow>
         </TableHead>
         <TableBody>
           {restaurantes.map((restaurante) => (
             <TableRow key={restaurante.id}>
               <TableCell>{restaurante.nome}</TableCell>
+              <TableCell>
+                [{' '}
+                <Link to={`/admin/restaurantes/${restaurante.id}`}>Editar</Link>{' '}
+                ]
+              </TableCell>
+              <TableCell>
+                <Button
+                  variant="outlined"
+                  color="error"
+                  onClick={() => excluir(restaurante)}
+                >
+                  {' '}
+                  Excluir{' '}
+                </Button>
+              </TableCell>
             </TableRow>
           ))}
         </TableBody>
--- src/paginas/Administracao/Restaurantes/FomularioRestaurante.tsx
+++ src/paginas/Administracao/Restaurantes/FomularioRestaurante.tsx
@@ -1,32 +1,75 @@
-import { Button, TextField } from '@mui/material';
+import { Box, Button, TextField, Typography } from '@mui/material';
 import axios from 'axios';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
+import IRestaurante from '../../../interfaces/IRestaurante';
 
 const FormularioRestaurante = () => {
+  const parametros = useParams();
+  useEffect(() => {
+    if (parametros.id) {
+      console.log(parametros.id);
+      axios
+        .get<IRestaurante>(
+          `http://localhost:8000/api/v2/restaurantes/${parametros.id}/`
+        )
+        .then((response) => setRestaurante(response.data.nome));
+    }
+  }, [parametros]);
   const [nomeRestaurante, setRestaurante] = useState('');
   const aoSubmeterForm = (evento: React.FormEvent<HTMLFormElement>) => {
     evento.preventDefault();
-    axios
-      .post('http://localhost:8000/api/v2/restaurantes/', {
-        nome: nomeRestaurante,
-      })
-      .then((response) => {
-        alert('Restaurante cadastrado com sucesso');
-      });
+    if (parametros.id) {
+      axios
+        .put(`http://localhost:8000/api/v2/restaurantes/${parametros.id}/`, {
+          nome: nomeRestaurante,
+        })
+        .then((response) => {
+          alert('Restaurante atualizado com sucesso');
+        });
+    } else {
+      axios
+        .post('http://localhost:8000/api/v2/restaurantes/', {
+          nome: nomeRestaurante,
+        })
+        .then((response) => {
+          alert('Restaurante cadastrado com sucesso');
+        });
+    }
   };
   return (
-    <form onSubmit={aoSubmeterForm}>
-      <TextField
-        value={nomeRestaurante}
-        onChange={(evento) => setRestaurante(evento.target.value)}
-        id="nome"
-        label="nome"
-        variant="standard"
-      ></TextField>
-      <Button type="submit" variant="outlined">
-        Enviar
-      </Button>
-    </form>
+    <Box
+      sx={{
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+        justifyContent: 'center',
+      }}
+    >
+      <Typography component="h1" variant="h6">
+        Formulário de Restaurantes
+      </Typography>
+      <Box component="form" onSubmit={aoSubmeterForm}>
+        <TextField
+          value={nomeRestaurante}
+          onChange={(evento) => setRestaurante(evento.target.value)}
+          id="nome"
+          label="nome"
+          variant="standard"
+          fullWidth
+          required
+          sx={{ marginTop: 2 }}
+        ></TextField>
+        <Button
+          sx={{ marginTop: 1 }}
+          type="submit"
+          variant="outlined"
+          fullWidth
+        >
+          Enviar
+        </Button>
+      </Box>
+    </Box>
   );
 };
 
